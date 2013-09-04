c4dd335693bfe42c56d1e38300aed61bb43bbf96
bhawkeswood <bhawkeswood@gmail.com>
2013-09-04 16:06:00 -0400

    Refactoring to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,57 +1,27 @@
 class PicturesController < ApplicationController
 
 	def index
-	    @pictures = [
-	      {
-	        :title  => "The old church on the coast of White sea",
-	        :artist => "Sergey Ershov",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	      },
-	      {
-	        :title  => "Sea Power",
-	        :artist => "Stephen Scullion",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	      },
-	      {
-	        :title  => "Into the Poppies",
-	        :artist => "John Wilhelm",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	      }
-	    ]
+	    @pictures = Picture.all
     end
 
     def show
-	    @pictures = [
-	      {
-	        :title  => "The old church on the coast of White sea",
-	        :artist => "Sergey Ershov",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	      },
-	      {
-	        :title  => "Sea Power",
-	        :artist => "Stephen Scullion",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	      },
-	      {
-	        :title  => "Into the Poppies",
-	        :artist => "John Wilhelm",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	      }
-	    ]
-	    @picture = @pictures[params[:id].to_i]
+	    @pictures = Picture.find(params[:id])
     end
 
     def new
+    	@picture = Picture.new
     end
 
     def create
-	# 	@picture = Picture.new(params.require(:picture).permit(:title, :artist, :url))
-	# 		if @picture.save
-	# 		redirect_to pictures_url
-	# 		else
-	# 		render :new
-	# 		end
-	# end
-    	render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+		@picture = Picture.new(params.require(:picture).permit(:title, :artist, :url))
+			if @picture.save
+			redirect_to pictures_url
+			else
+			render :new
+			end
     end	
+
+    def picture_params
+    	params.require(:picture).permit(:artist, :title, :url)
+    end
 end
