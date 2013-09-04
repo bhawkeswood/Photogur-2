ac03ef6852e67e1527f1d8ae6c8b81c252eecf19
bhawkeswood <bhawkeswood@gmail.com>
2013-09-04 14:53:02 -0400

    Photogur 2 Making eachpicture show at it's own url using static routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Photogur::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 get 'pictures' => 'pictures#index'
+
+get 'pictures/0' => 'pictures#picture0'
+get 'pictures/1' => 'pictures#picture1'
+get 'pictures/2' => 'pictures#picture2'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
